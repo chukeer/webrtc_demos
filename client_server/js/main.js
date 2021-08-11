6fcdef0a21c3f4758b1b5724cdc762b9efce93d8
zhangmenghan <>
2021-08-11 22:26:40 +0800

    client Server合为一个页面

---
+++ client_server/js/main.js
@@ -0,0 +1,137 @@
+import SignalChannel from "./signal_channel.js"
+
+let clientName = "client"
+let serverName = "server"
+let roomId = Date.now()
+console.log(`roomId:${roomId}`)
+
+const constraints = window.constraints = {
+  audio: false,
+  video: true
+};
+
+function createCtx(userName) {
+  let ctx = new Object()
+
+  let signalChannel = new SignalChannel(roomId, userName)
+  signalChannel.onSdp = msg => onRemoteSdp(ctx, msg)
+  signalChannel.onCandidate = msg => onRemoteCandidate(ctx, msg)
+
+  const configuration = {};
+  let pc = new RTCPeerConnection(configuration);
+
+  ctx.userName = userName
+  ctx.signalChannel = signalChannel
+  ctx.pc = pc
+
+  return ctx
+}
+
+async function startClient(e) {
+  e.target.disabled = true;
+  let ctx = createCtx(clientName)
+
+  let stream = await navigator.mediaDevices.getUserMedia(constraints);
+  const video = document.querySelector('#localVideo');
+  const videoTracks = stream.getVideoTracks();
+  console.log(`[${ctx.userName}] Got stream with constraints:${constraints}`);
+  console.log(`[${ctx.userName}] Using video device: ${videoTracks[0].label}`);
+
+  video.srcObject = stream;
+
+  ctx.pc.addEventListener('icecandidate', e => onIceCandidate(ctx, e));
+  ctx.pc.addEventListener('iceconnectionstatechange', e => onIceStateChange(ctx, e));
+
+  stream.getTracks().forEach(track => ctx.pc.addTrack(track, stream));
+  console.log(`[${ctx.userName}] Added local stream to pc`);
+
+  try {
+    const offer = await ctx.pc.createOffer()
+    await onCreateOfferSuccess(ctx, offer)
+  } catch (e) {
+    console.log(`[${ctx.userName}] Failed to set session description: ${e.toString()}`);
+  }
+}
+
+async function startServer(e) {
+  e.target.disabled = true;
+  let ctx = createCtx(serverName)
+
+  ctx.pc.addEventListener('icecandidate', e => onIceCandidate(ctx, e));
+  ctx.pc.addEventListener('iceconnectionstatechange', e => onIceStateChange(ctx, e));
+  ctx.pc.addEventListener('track', e => gotRemoteStream(ctx, e));
+}
+
+async function onCreateOfferSuccess(ctx, desc) {
+  console.log(`[${ctx.userName}] pc setLocalDescription start, sdp:\n${desc.sdp}`);
+  try {
+    await ctx.pc.setLocalDescription(desc);
+    console.log(`[${ctx.userName}] setLocalDescription complete`);
+    ctx.signalChannel.sendSdp(JSON.stringify(desc))
+  } catch (e) {
+    console.log(`[${ctx.userName}] Failed to set session description: ${e.toString()}`);
+  }
+}
+
+function onIceCandidate(ctx, event) {
+  console.log(`[${ctx.userName}] ICE candidate:\n${event.candidate ? JSON.stringify(event.candidate) : '(null)'}`);
+  if (event.candidate) {
+    ctx.signalChannel.sendCandidate(JSON.stringify(event.candidate))
+  }
+}
+
+function onIceStateChange(ctx, event) {
+  console.log(`[${ctx.userName}] ICE state: ${ctx.pc.iceConnectionState}`);
+  console.log(`[${ctx.userName}] ICE state change event: ${event}`);
+}
+
+async function onRemoteSdp(ctx, msg) {
+  console.log(`[${ctx.userName}] onRemoteSdp:${msg}`)
+  try {
+    let remoteSdp = JSON.parse(msg) 
+    await ctx.pc.setRemoteDescription(remoteSdp);
+    console.log(`[${ctx.userName}] setRemoteDescription complete`);
+
+    if (ctx.userName == serverName) {
+      console.log(`[${ctx.userName}] createAnswer start`);
+      const answer = await ctx.pc.createAnswer();
+      await onCreateAnswerSuccess(ctx, answer)
+    }
+  } catch (e) {
+    console.log(`[${ctx.userName}] Failed to set session description: ${e.toString()}`);
+  }
+}
+
+async function onCreateAnswerSuccess(ctx, desc) {
+  console.log(`[${ctx.userName}] Answer from pc:\n${desc.sdp}`);
+  console.log(`[${ctx.userName}] setLocalDescription start`);
+  try {
+    await ctx.pc.setLocalDescription(desc);
+    console.log(`[${ctx.userName}] setLocalDescription complete`);
+    ctx.signalChannel.sendSdp(JSON.stringify(desc))
+  } catch (e) {
+    console.log(`[${ctx.userName}] Failed to set session description: ${e.toString()}`);
+  }
+}
+
+async function onRemoteCandidate(ctx, msg) {
+  console.log(`[${ctx.userName}] onRemoteCandidate: ${msg}`)
+  try {
+    let remoteCandidate = JSON.parse(msg)
+    await ctx.pc.addIceCandidate(new RTCIceCandidate(remoteCandidate));
+    console.log(`[${ctx.userName}] addIceCandidate success`);
+  } catch (e) {
+    console.log(`[${ctx.userName}] failed to add ICE Candidate: ${e.toString()}`);
+  }
+}
+
+function gotRemoteStream(ctx, e) {
+  const remoteVideo = document.getElementById('remoteVideo');
+  if (remoteVideo.srcObject !== e.streams[0]) {
+    remoteVideo.srcObject = e.streams[0];
+    console.log(`[${ctx.userName}] received remote stream`);
+  }
+}
+
+document.querySelector('#startClient').addEventListener('click', e => startClient(e));
+document.querySelector('#startServer').addEventListener('click', e => startServer(e));
--- client_server/js/signal_channel.js
+++ client_server/js/signal_channel.js
@@ -1,10 +1,9 @@
 export default class SignalChannel {
-    constructor(roomId, userName, onSdp, onCandidate) {
+    onSdp
+    onCandidate
+    constructor(roomId, userName) {
         this.roomId = roomId
         this.userName = userName
-        this.onSdp = onSdp
-        this.onCandidate = onCandidate
-        console.log("constructor room id:" + roomId)
         this.ws = new WebSocket("wss://192.168.3.205:8080")
         this.ws.onmessage = event => this.onmessage(event)
         this.ws.onopen = event => this.login()
@@ -13,26 +12,26 @@ export default class SignalChannel {
     login() {
         let msg = {room_id: this.roomId, user_name: this.userName, type: "login"}
         let str = JSON.stringify(msg)
-        console.log("login:" + str)
+        console.log(`[${this.userName}] login: ${str}`)
         this.ws.send(str)
     }
 
     sendSdp(desc) {
         let msg = {room_id: this.roomId, user_name: this.userName, type: "sdp", msg: desc}
         let str = JSON.stringify(msg)
-        console.log("sendSdp:" + str)
+        console.log(`[${this.userName}] sendSdp: ${str}`)
         this.ws.send(str)
     }
 
     sendCandidate(candidate) {
         let msg = {room_id: this.roomId, user_name: this.userName, type: "candidate", msg: candidate}
         let str = JSON.stringify(msg)
-        console.log("sendCandidate:" + str)
+        console.log(`[${this.userName}] sendCandidate: ${str}`)
         this.ws.send(str)
     }
 
     onmessage(event) {
-        console.log("recv message:" + event.data)
+        console.log(`[${this.userName}] recv message: ${event.data}`)
         var msg = JSON.parse(event.data)
         if (msg.type == "sdp") {
             if (this.onSdp) {
